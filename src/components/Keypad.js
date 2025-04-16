e85573dac1bfdc54a09d490de22c64c207fea757
ian <ianwisdom90@gmail.com>
2025-04-17 00:58:15 +0300

    EyeKeys

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
-// Code Keypad Component Here
+import React from 'react'
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+
+  function handleChange() {
+    console.log("Entering password...")
+  }
+
+  return (
+    <input type="password" onChange={handleChange} />
+  )
 }
 
-export default Keypad;
+export default Keypad
